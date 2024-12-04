fc11459f228de50ddd37d1ff3aaaaa250a7d8c5c
sreehb-123 <sreeh190206@gmail.com>
2024-12-04 10:05:48 +0530

    minor 2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import Navbar from './components/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
